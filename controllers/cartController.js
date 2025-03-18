e219825a3a3ff6219ad1e632ae7e3e6a561f228e
remeessalam <remeesreme4u@gmail.com>
2025-03-18 17:42:07 +0530

    add status for cart api

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -66,15 +66,16 @@ export const removeFromCart = async (req, res) => {
   const { productId } = req.params;
   try {
     let cart = await Cart.findOne({ user: req.user.userId });
-    if (!cart) return res.status(404).json({ message: "Cart not found" });
+    if (!cart)
+      return res.status(404).json({ status: false, message: "Cart not found" });
 
     cart.items = cart.items.filter(
       (item) => item.product.toString() !== productId
     );
     await cart.save();
     await cart.populate("items.product");
-    res.status(200).json(cart);
+    res.status(200).json({ status: true, cart });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ status: false, message: err.message });
   }
 };
